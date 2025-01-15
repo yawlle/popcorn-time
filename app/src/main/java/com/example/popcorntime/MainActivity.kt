7d1948e3c889606459d2c056ad8f7d15b0efa96d
Isabella <isabellapinheiro@primelan.com.br>
2025-01-15 13:47:16 -0300

    first commit

---
+++ app/src/main/java/com/example/popcorntime/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.popcorntime
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+
+}
---
+++ app/src/main/java/com/example/popcorntime/presentation/fragment/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.popcorntime.presentation.fragment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.popcorntime.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.lottieView.enableMergePathsForKitKatAndAbove(true)
+        binding.lottieView.playAnimation()
+
+    }
+}
